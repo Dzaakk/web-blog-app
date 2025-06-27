993277a03a3fd5555e7f28098e4a3207482316bf
Dzkzzz <dzdzaakk@gmail.com>
2025-06-27 22:41:29 +0700

    css responsive layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,14 @@
-import styles from "./homepage.module.css";
-
+import Link from "next/link";
+// import style from "./homepage.module.css"
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <Link href="/">
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </h1>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </Link>
+  </div>;
 }
